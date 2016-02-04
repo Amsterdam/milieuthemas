72c85845a8f0ab2927a8539e890560e73607cc27
Richard Jansen <richard.jansen@lajos.nl>
2016-02-04 10:47:44 +0100

    ATLAS-978 geoviews schiphol

--- web/atlas_milieuthemas/datasets/schiphol/tests/factories.py
+++ web/atlas_milieuthemas/datasets/schiphol/tests/factories.py
@@ -1,11 +1,52 @@
 import factory
 from factory import fuzzy
 
+from django.contrib.gis.geos import Point, LineString, MultiPolygon, Polygon
+
 from .. import models
 
 
-class MeetboutFactory(factory.DjangoModelFactory):
+class HoogtebeperkendeVlakkenPointFactory(factory.DjangoModelFactory):
     class Meta:
         model = models.HoogtebeperkendeVlakken
 
-    id = fuzzy.FuzzyText(length=10)
+    id = fuzzy.FuzzyInteger(0, 10)
+    geo_id = fuzzy.FuzzyInteger(0, 10)
+    geometrie_point = Point(0.0, 1.1)
+
+
+class HoogtebeperkendeVlakkenLineFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.HoogtebeperkendeVlakken
+
+    id = fuzzy.FuzzyInteger(0, 10)
+    geo_id = fuzzy.FuzzyInteger(0, 10)
+    geometrie_line = LineString((0, 0), (1, 1))
+
+
+class HoogtebeperkendeVlakkenPolyFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.HoogtebeperkendeVlakken
+
+    id = fuzzy.FuzzyInteger(0, 10)
+    geo_id = fuzzy.FuzzyInteger(0, 10)
+    geometrie_polygon = MultiPolygon(
+        Polygon(((0, 0), (0, 1), (1, 1), (0, 0))),
+        Polygon(((1, 1), (1, 2), (2, 2), (1, 1)))
+    )
+
+
+class GeluidzoneFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Geluidzone
+
+    id = fuzzy.FuzzyInteger(0, 10)
+    geo_id = fuzzy.FuzzyInteger(0, 10)
+
+
+class VogelvrijwaringsgebiedFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = models.Geluidzone
+
+    id = fuzzy.FuzzyInteger(0, 10)
+    geo_id = fuzzy.FuzzyInteger(0, 10)
--- web/atlas_milieuthemas/geo_views/tests/test_views.py
+++ web/atlas_milieuthemas/geo_views/tests/test_views.py
@@ -2,6 +2,8 @@ from django.db import connection
 
 from django.test import TestCase
 
+from datasets.schiphol.tests import factories as schiphol_factories
+
 
 class ViewsTest(TestCase):
     def get_row(self, view_name):
@@ -11,3 +13,21 @@ class ViewsTest(TestCase):
         self.assertIsNotNone(result)
 
         return dict(zip([col[0] for col in cursor.description], result))
+
+    def test_schiphol_hoogte_point(self):
+        l = schiphol_factories.HoogtebeperkendeVlakkenPointFactory.create()
+        row = self.get_row('geo_schiphol_hoogte_point_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_schiphol_hoogte_line(self):
+        l = schiphol_factories.HoogtebeperkendeVlakkenLineFactory.create()
+        row = self.get_row('geo_schiphol_hoogte_line_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_schiphol_hoogte_poly(self):
+        l = schiphol_factories.HoogtebeperkendeVlakkenPolyFactory.create()
+        row = self.get_row('geo_schiphol_hoogte_polygon_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['geometrie'], None)
