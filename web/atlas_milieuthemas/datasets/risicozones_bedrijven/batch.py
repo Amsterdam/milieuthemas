d1a37556952fc0e9a7ba5c671d1c4bcc97a92361
Kfir Breger <kfir@cubicalwave.com>
2016-02-17 10:27:08 +0100

    Clarifing import sources

--- web/atlas_milieuthemas/datasets/risicozones_bedrijven/batch.py
+++ web/atlas_milieuthemas/datasets/risicozones_bedrijven/batch.py
@@ -1,14 +1,14 @@
+# Python
 import logging
 import os
-
-from django.contrib.gis.geos import GEOSGeometry, Point, Polygon, MultiPolygon
+# Packages
+from django.contrib.gis.geos import GEOSGeometry, MultiPolygon, Point, Polygon
 from django.contrib.gis.geos.error import GEOSException
-
+# Project
+from . import models
 from datapunt_generic.batch import batch
 from datapunt_generic.generic import database
-
 from datapunt_generic.generic.csv import process_csv, parse_nummer
-from . import models
 
 log = logging.getLogger(__name__)
 
--- web/atlas_milieuthemas/datasets/risicozones_bedrijven/models.py
+++ web/atlas_milieuthemas/datasets/risicozones_bedrijven/models.py
@@ -1,6 +1,7 @@
+# Project
 from django.db import models
 from django.contrib.gis.db import models as geo
-
+# Product
 from datapunt_generic.generic import mixins
 
 
