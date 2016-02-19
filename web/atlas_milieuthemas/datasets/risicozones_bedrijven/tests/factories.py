50d6f67d184928591c1c37a1dcfdff6f685e9070
Richard Jansen <richard.jansen@lajos.nl>
2016-02-19 16:31:32 +0100

    ATLAS-954 missing geo-views risicozones_bedrijven

--- web/atlas_milieuthemas/datasets/risicozones_bedrijven/tests/factories.py
+++ web/atlas_milieuthemas/datasets/risicozones_bedrijven/tests/factories.py
@@ -1,9 +1,76 @@
 import factory
+from factory import fuzzy
+
+from django.contrib.gis.geos import Point, MultiPolygon, Polygon
 
 from .. import models
 
+multipoly = MultiPolygon(
+    Polygon(((0, 0), (0, 1), (1, 1), (0, 0))),
+    Polygon(((1, 1), (1, 2), (2, 2), (1, 1)))
+)
 
-class LPGStationFactory(factory.DjangoModelFactory):
+
+class IdMixin(factory.DjangoModelFactory):
+    id = fuzzy.FuzzyInteger(1, 10)
+
+
+# station
+class LPGStationBase(IdMixin):
+    bedrijfsnaam = fuzzy.FuzzyText(length=40)
+    dossiernummer = fuzzy.FuzzyText(length=40)
+
     class Meta:
         model = models.LPGStation
 
+
+class LPGStationPointFactory(LPGStationBase):
+    geometrie_point = Point(0.0, 1.1)
+
+
+class LPGStationPolygonFactory(LPGStationBase):
+    geometrie_polygon = multipoly
+
+
+# vulpunt
+class LPGVulpuntBase(IdMixin):
+    geo_id = fuzzy.FuzzyInteger(1, 10)
+    type = fuzzy.FuzzyText(length=40)
+    afstandseis = fuzzy.FuzzyText(length=10)
+    voldoet = fuzzy.FuzzyText(length=3)
+
+    class Meta:
+        model = models.LPGVulpunt
+
+
+class LPGVulpuntPointFactory(LPGVulpuntBase):
+    geometrie_point = Point(0.0, 1.1)
+
+
+class LPGVulpuntPolygonFactory(LPGVulpuntBase):
+    geometrie_polygon = multipoly
+
+
+class LPGTankFactory(factory.DjangoModelFactory):
+    type = fuzzy.FuzzyText(length=40)
+    afstandseis = fuzzy.FuzzyText(length=10)
+    voldoet = fuzzy.FuzzyText(length=3)
+    geometrie = multipoly
+
+    class Meta:
+        model = models.LPGTank
+
+
+class LPGAfleverzuilBase(factory.DjangoModelFactory):
+    station_id = fuzzy.FuzzyInteger(0, 10)
+
+    class Meta:
+        model = models.LPGAfleverzuil
+
+
+class LPGAfleverzuilPointFactory(LPGAfleverzuilBase):
+    geometrie_point = Point(0.0, 1.1)
+
+
+class LPGAfleverzuilPolygonFactory(LPGAfleverzuilBase):
+    geometrie_polygon = multipoly
--- web/atlas_milieuthemas/geo_views/tests/test_views.py
+++ web/atlas_milieuthemas/geo_views/tests/test_views.py
@@ -1,3 +1,4 @@
+from unittest import skip
 from django.db import connection
 
 from django.test import TestCase
@@ -5,6 +6,7 @@ from django.test import TestCase
 from datasets.schiphol.tests import factories as schiphol_factories
 from datasets.bodeminformatie.tests import factories as bodeminformatie_factories
 from datasets.veiligheidsafstanden.tests import factories as veiligheidsafstanden_factories
+from datasets.risicozones_bedrijven.tests import factories as risicozones_bedrijven_factories
 
 
 class ViewsTest(TestCase):
@@ -16,6 +18,7 @@ class ViewsTest(TestCase):
 
         return dict(zip([col[0] for col in cursor.description], result))
 
+    # schiphol
     def test_schiphol_hoogte_point(self):
         l = schiphol_factories.HoogtebeperkendeVlakkenPointFactory.create()
         row = self.get_row('geo_schiphol_hoogte_point_layer')
@@ -46,6 +49,7 @@ class ViewsTest(TestCase):
         self.assertEqual(row['id'], l.id)
         self.assertNotEqual(row['geometrie'], None)
 
+    # bodeminformatie
     def test_bodeminformatie_grondmonster(self):
         l = bodeminformatie_factories.GrondmonsterFactory.create()
         row = self.get_row('geo_bodeminformatie_grondmonster')
@@ -67,6 +71,7 @@ class ViewsTest(TestCase):
         self.assertNotEqual(row['concentratie'], None)
         self.assertNotEqual(row['geometrie'], None)
 
+    # veiligheidsafstanden
     def test_veiligheidsafstanden_point(self):
         l = veiligheidsafstanden_factories.VeiligheidsafstandPointFactory.create()
         row = self.get_row('geo_veiligheidsafstanden_point_layer')
@@ -81,4 +86,62 @@ class ViewsTest(TestCase):
         self.assertEqual(row['id'], l.id)
         self.assertNotEqual(row['type'], None)
         self.assertNotEqual(row['locatie'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    # risicozones_bedrijven
+    def test_risicozones_bedrijven_lpgstation_point_layer(self):
+        l = risicozones_bedrijven_factories.LPGStationPointFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgstation_point_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['bedrijfsnaam'], None)
+        self.assertNotEqual(row['dossiernummer'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_risicozones_bedrijven_lpgstation_polygon_layer(self):
+        l = risicozones_bedrijven_factories.LPGStationPolygonFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgstation_polygon_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['bedrijfsnaam'], None)
+        self.assertNotEqual(row['dossiernummer'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_risicozones_bedrijven_lpgvulpunt_point_layer(self):
+        l = risicozones_bedrijven_factories.LPGVulpuntPointFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgvulpunt_point_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['type'], None)
+        self.assertNotEqual(row['afstandseis'], None)
+        self.assertNotEqual(row['voldoet'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_risicozones_bedrijven_lpgvulpunt_polygon_layer(self):
+        l = risicozones_bedrijven_factories.LPGVulpuntPolygonFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgvulpunt_polygon_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['type'], None)
+        self.assertNotEqual(row['afstandseis'], None)
+        self.assertNotEqual(row['voldoet'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_risicozones_bedrijven_lpgtank(self):
+        l = risicozones_bedrijven_factories.LPGTankFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgtank')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['type'], None)
+        self.assertNotEqual(row['afstandseis'], None)
+        self.assertNotEqual(row['voldoet'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_risicozones_bedrijven_lpgafleverzuil_point_layer(self):
+        l = risicozones_bedrijven_factories.LPGAfleverzuilPointFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgafleverzuil_point_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['station_id'], None)
+        self.assertNotEqual(row['geometrie'], None)
+
+    def test_risicozones_bedrijven_lpgafleverzuil_polygon_layer(self):
+        l = risicozones_bedrijven_factories.LPGAfleverzuilPolygonFactory.create()
+        row = self.get_row('geo_risicozones_bedrijven_lpgafleverzuil_polygon_layer')
+        self.assertEqual(row['id'], l.id)
+        self.assertNotEqual(row['station_id'], None)
         self.assertNotEqual(row['geometrie'], None)
