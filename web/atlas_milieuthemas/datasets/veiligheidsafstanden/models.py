b315d39a49876c625d5c1d238a0600fa07642697
Richard Jansen <richard.jansen@lajos.nl>
2016-02-22 11:34:47 +0100

    ATLAS-958 display column for LPGStation geoview

--- web/atlas_milieuthemas/datasets/veiligheidsafstanden/models.py
+++ web/atlas_milieuthemas/datasets/veiligheidsafstanden/models.py
@@ -5,8 +5,6 @@ from datapunt_generic.generic import mixins
 from datasets.themas.models import Thema
 
 
-# TODO:
-# discuss having one model with different geo fields and features types vs model per feature type vs model per geo type
 class Veiligheidsafstand(mixins.ImportStatusMixin):
     geo_id = models.IntegerField(null=False)
     type = models.CharField(max_length=100, null=True)
