8e221f9577004e0a4d3e046d803240b433e0c9be
Kfir Breger <kfir@cubicalwave.com>
2016-02-25 10:55:23 +0100

    ATLAS-913 tests now cover new data model

--- web/atlas_milieuthemas/datasets/risicozones_infrastructuur/tests/test_batch.py
+++ web/atlas_milieuthemas/datasets/risicozones_infrastructuur/tests/test_batch.py
@@ -3,6 +3,34 @@ from datapunt_generic.batch.test import TaskTestCase
 from .. import batch, models
 
 
+class ImportAardgasleidingTest(object):
+    def task(self):
+        return batch.ImportAardgasleidingTask()
+
+    def test_import(self):
+        self.run_task()
+
+        imported = models.Aardgasleiding.objects.count()
+        self.assertEqual(9, imported)
+        
+        entry = models.Aardgasleiding.objects.get(pk=3)
+        assertNotEqual(entry.geometrie, None)
+
+class ImportImportAardgasGebiedTest(object):
+    def task(self):
+        return batch.ImportAardgasGebiedTask()
+
+    def test_import(self):
+        self.run_task()
+
+        # Checking result per type
+        results = (('la', 1), ('l1', 1), ('pr', 11), ('zk', 4))
+        for res in result:
+            imported = models.AardgasGebied.objects.filter(type=res[0]).count()
+            assertEqual(imported, res[1])
+
+        
+
 class ImportInfrastructuurTestBase(object):
     """
     Base infrastructuur test class
@@ -24,31 +52,24 @@ class ImportInfrastructuurTestBase(object):
 
         entry = models.Infrastructuur.objects.filter(type=self.infrastructuur_type).first()
         self.assertNotEqual(getattr(entry, self.geos_val, None), None)
-    
-    
-class ImportAardgasbuisleidingTest(ImportInfrastructuurTestBase, TaskTestCase):
-    task_name = batch.ImportAardgasbuisleidingTask
-    infrastructuur_type = 'ag'
-    exp_count = 9
-    geos_val = 'geometrie_line'
 
 
 class ImportSpoorwegTest(ImportInfrastructuurTestBase, TaskTestCase):
     task_name = batch.ImportSpoorwegTask
     infrastructuur_type = 'sw'
     exp_count = 1
-    geos_val = 'geometrie_polygon'
+    geos_val = 'geometrie'
 
 
 class ImportVaarpwegTest(ImportInfrastructuurTestBase, TaskTestCase):
     task_name = batch.ImportVaarwegTask
     infrastructuur_type = 'vw'
     exp_count = 1
-    geos_val = 'geometrie_polygon'
+    geos_val = 'geometrie'
 
 
 class ImportWegTest(ImportInfrastructuurTestBase, TaskTestCase):
     task_name = batch.ImportWegTask
     infrastructuur_type = 'wg'
     exp_count = 1
-    geos_val = 'geometrie_polygon'
+    geos_val = 'geometrie'
