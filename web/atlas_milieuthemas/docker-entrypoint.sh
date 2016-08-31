68d10bcfe281fdf47432287bf944195046764896
Stephan Zaat <s.zaat@amsterdam.nl>
2016-08-31 15:36:22 +0200

    Revert to previous entrypoint

--- web/atlas_milieuthemas/docker-entrypoint.sh
+++ web/atlas_milieuthemas/docker-entrypoint.sh
@@ -8,5 +8,14 @@ cd /app
 # collect static files
 python manage.py collectstatic --noinput
 
+# migrate database tables
+yes yes | python manage.py migrate --noinput
+
+# run import
+python manage.py run_import
+
+# sync geo views
+python manage.py sync_views
+
 # run uwsgi
 exec uwsgi --ini /app/uwsgi.ini
