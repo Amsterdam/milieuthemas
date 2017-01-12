55383d7c79ead557d0d64ebe7a004b9438dbca9e
Jeroen van 't Hert <jeroen@yottabyte.io>
2017-01-12 12:45:14 +0100

    Deployment to Docker with new standards.

--- web/milieuthemas/docker-entrypoint.sh
+++ web/milieuthemas/docker-entrypoint.sh
@@ -3,6 +3,24 @@
 set -u
 set -e
 
+METADATA_HOST=169.254.169.254
+METADATA_PORT=80
+METADATA_URL=http://${METADATA_HOST}:${METADATA_PORT}/latest/meta-data/local-ipv4
+
+export DOCKER_IP=$(hostname --ip-address)
+
+#Check connectivity
+curl -s -m1 ${METADATA_URL} || exitcode=$?
+if [ -n "${exitcode+set}" ]; then
+  export DOCKER_EXTERNAL_IP=${DOCKER_IP}
+else
+  export DOCKER_EXTERNAL_IP=$(curl ${METADATA_URL})
+fi
+
+if [ "${CONSUL_HOST:-notset}" == 'notset' ]; then
+    export CONSUL_HOST=${DOCKER_EXTERNAL_IP}
+fi
+
 cd /app
 
 # collect static files
