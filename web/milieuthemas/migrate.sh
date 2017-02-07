dd98b7240fb54626bd160ed5d9f9349a093e8353
Kfir Breger <kfir@cubicalwave.com>
2017-02-07 12:20:38 +0100

    Trying to avoind view problem

--- web/milieuthemas/migrate.sh
+++ web/milieuthemas/migrate.sh
@@ -3,5 +3,5 @@
 set -u   # crash on missing env variables
 set -e   # stop on any error
 
-yes yes | python manage.py migrate geo_views zero --noinput
+# yes yes | python manage.py migrate geo_views zero --noinput
 yes yes | python manage.py migrate --noinput
