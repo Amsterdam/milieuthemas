f629024efaf52d0f566aa7cb9152b0411316cc4c
Kfir Breger <kfir@cubicalwave.com>
2017-02-10 11:43:51 +0100

    tg-724 Import process handles database migration and view creation. Deployment does not need to

--- web/milieuthemas/docker-entrypoint.sh
+++ web/milieuthemas/docker-entrypoint.sh
@@ -26,11 +26,5 @@ cd /app
 # collect static files
 python manage.py collectstatic --noinput
 
-# migrate database tables
-#yes yes | python manage.py migrate --noinput
-
-# sync geo views
-# python manage.py sync_views
-
 # run uwsgi
 exec uwsgi --ini /app/uwsgi.ini
